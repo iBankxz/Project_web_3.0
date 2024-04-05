a991c42f1fe50f6d77c9b636fb147d1f94949b46
iBankxz <dylanmolinamazzilli@gmail.com>
2024-04-04 21:29:02 -0500

    add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu/Menu";
+import About from "./components/About/About";
+import Products from "./components/Products/Products";
+import Contact from "./components/Contact/Contact";
 
 
 function App (){
@@ -7,9 +10,9 @@ function App (){
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Menu/>}></Route>
-        <Route path="/about" element={<div>Admin</div>}></Route>
-        <Route path="/products" element={<div>Admin</div>}></Route>
-        <Route path="/contact" element={<div>Admin</div>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/products" element={<Products/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
       </Routes>
     </BrowserRouter>
   )
